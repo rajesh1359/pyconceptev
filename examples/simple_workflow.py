77b819c5041cfbde9fc6144e57e9bd979ba6d115
Philip Usher <philip.usher@ansys.com>
2024-07-11 11:58:33 +0100

    slowed down pinging the results server

--- examples/simple_workflow.py
+++ examples/simple_workflow.py
@@ -238,7 +238,7 @@ with app.get_http_client(token, design_instance_id) as client:
     job_info = app.create_submit_job(client, concept, account_id, hpc_id)
 
     # Read the results and show the result in your browser
-    results = app.read_results(client, job_info, calculate_units=False)
+    results = app.read_results(client, job_info, calculate_units=False, rate_limit=3)
     x = results[0]["capability_curve"]["speeds"]
     y = results[0]["capability_curve"]["torques"]
 
